c340082c6159cc25b941a98d1a38d3a920979fdd
sakuraimisato <blueberry112358@gmail.com>
2024-02-03 00:35:15 +0900

    first commit I will combine quiz pages

--- カフェ謎/rpglike.js
+++ カフェ謎/rpglike.js
@@ -1,6 +1,6 @@
 function openQ(n){
-    const quiz = document.getElementById(n)
-    const quiz_back = quiz.parentElement
+    const quiz = document.getElementById(n);
+    const quiz_back = quiz.parentElement;
     quiz.style.display = 'block';
     quiz_back.style.display = 'block';
 }
@@ -13,5 +13,26 @@ function closeQ(n){
 }
 
 function checkA(n){
-    const quiz = document.getElementById(n)
+    const players_answer = document.getElementById(n).anser_box;
+    console.log(players_answer);
+    
+}
+
+
+var quiz ={
+    "Q1":{
+        quiz_image : "/images/pro_quiz_image",
+        answer:"答え1", 
+        point: 10
+    },
+    "Q2":{
+        quiz_image : "/images/pro_quiz_image", 
+        answer:"答え2", 
+        point: 20
+    },
+    "Q3":{
+        quiz_image : "/images/pro_quiz_image", 
+        answer:"答え3", 
+        point: 30
+    }   
 }
